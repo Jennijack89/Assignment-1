29ecf37105ec2da2f9205d5b30410a44d87a9990
Jennifer Marshall <111827054+Jennijack89@users.noreply.github.com>
2022-10-10 19:31:04 +0000

    Added HTML form attributes and tested- psswrd-notworking

---
+++ script.js
@@ -0,0 +1,5 @@
+let passWord=document.getElementById('passWrd').value;
+let rePassWord=document.getElementById('passWrdreEntr').value;
+let result = passWord.match(rePassWord)
+if result = false
+  alert
