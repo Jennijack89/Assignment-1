87f6625d3c3d1b6279016824b711e2f7ecfe1a21
Jennifer Marshall <111827054+Jennijack89@users.noreply.github.com>
2022-10-13 20:34:20 +0000

    Added Working Password Pattern and Working verifyPW() Function

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-let passWord=document.getElementById('passWrd').value;
-let rePassWord=document.getElementById('passWrdreEntr').value;
-let result = passWord.match(rePassWord)
-if result = false
-  alert
+//let passWord=document.getElementById('passWrd').value;
+//let rePassWord=document.getElementById('passWrdreEntr').value;
+//let result = passWord.match(rePassWord)
+//if result = false
+ // alert
+
+//const phNumPattern = /[0-9]{3}-[0-9]{3}-[0-9]{4}/
+//let result= pattern.test(document.getElementById('phonenum').value)
+
+//if (result == false) {
+//alert('Please use the specified format');
+//}
+
+function verifyPw(){
+  let passWord=document.getElementById('passWrd').value;
+  let rePassWord=document.getElementById('passWrdreEntr').value;
+  if (rePassWord != passWord){
+    alert("Password Mismatch: Please re-enter Password");
+    return false;
+    
+    }
+
+//function showPwd() {
+ // let myInput=document.getElementsByTagName('input');
+ // let rePassWord=document.getElementById('passWrdreEntr').value;
+  //if(myInput.type === "password") {
+//myInput.type = "text";}
+  //}
+  }
+
+
+    
+  
+ // }
+//}
