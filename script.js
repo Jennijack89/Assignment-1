eabab36649e889bd5bde90f3993272f0eec77c52
Jennifer Marshall <111827054+Jennijack89@users.noreply.github.com>
2022-10-16 23:10:44 +0000

    I included some functions I'm working on

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function verifyPw(){
   if (rePassWord != passWord){
     alert("Password Mismatch: Please re-enter Password");
     return false;
-    
     }
+}
 
 //function showPwd() {
  // let myInput=document.getElementsByTagName('input');
@@ -26,10 +26,42 @@ function verifyPw(){
   //if(myInput.type === "password") {
 //myInput.type = "text";}
   //}
-  }
-
-
-    
-  
- // }
+//  }
+//function verifyUsr(){
+//const names= [Harry,Elizabeth,Shana];
+//let userName=document.getElementById('usrName').value;
+//  for(let name of names){
+ //   if(names[name] in userName){
+  //    alert('This name is taken');
+//      return false;
+ //   }
+//  }  
 //}
+
+//function verifyUser(){
+//  const names = [harry,Elizabeth,Shana];
+//  let userName= document.getElementById('usrName').value;
+//  names.forEach(myFunction);
+//}
+//function myFunction(value){
+  //if(value in userName){
+  //  alert('This name is taken');
+  //}
+//}
+
+//function checkUser(){
+ // let userName= document.getElementById('usrName').value;
+  //const names= [Harry,Elizabeth,Shana];
+  //for (let x in names){
+  //  if(userName.includes(names[x]))
+    //  alert('This name is taken')
+  //}
+//}
+
+function checkUser(){
+  let userName= document.getElementById('usrName').value;
+  const names= [Harry,Elizabeth,Shana];
+  if(names.includes(userName){
+      alert('This name is taken')
+  }
+}
