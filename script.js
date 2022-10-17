892b6ff62260b36a18bca330f493b1f9bb174e25
Jennifer Marshall <111827054+Jennijack89@users.noreply.github.com>
2022-10-17 01:52:11 +0000

    Added  CheckUser() function, tested. nav a{} & nav a:hover{}

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-//let passWord=document.getElementById('passWrd').value;
-//let rePassWord=document.getElementById('passWrdreEntr').value;
-//let result = passWord.match(rePassWord)
-//if result = false
- // alert
-
-//const phNumPattern = /[0-9]{3}-[0-9]{3}-[0-9]{4}/
-//let result= pattern.test(document.getElementById('phonenum').value)
-
-//if (result == false) {
-//alert('Please use the specified format');
-//}
+function checkUser(){
+  let userName= document.getElementById('usrName').value;
+  let lowerUserName = userName.toLowerCase();
+  const names= ["harry","elizabeth","shana"];
+  if(names.includes(lowerUserName)){
+     alert('Username Exists: Please Choose Another.');
+    return false;
+  }
+  return true
+}
 
 function verifyPw(){
   let passWord=document.getElementById('passWrd').value;
@@ -18,50 +16,11 @@ function verifyPw(){
     alert("Password Mismatch: Please re-enter Password");
     return false;
     }
+  return true
 }
-
 //function showPwd() {
  // let myInput=document.getElementsByTagName('input');
  // let rePassWord=document.getElementById('passWrdreEntr').value;
   //if(myInput.type === "password") {
 //myInput.type = "text";}
   //}
-//  }
-//function verifyUsr(){
-//const names= [Harry,Elizabeth,Shana];
-//let userName=document.getElementById('usrName').value;
-//  for(let name of names){
- //   if(names[name] in userName){
-  //    alert('This name is taken');
-//      return false;
- //   }
-//  }  
-//}
-
-//function verifyUser(){
-//  const names = [harry,Elizabeth,Shana];
-//  let userName= document.getElementById('usrName').value;
-//  names.forEach(myFunction);
-//}
-//function myFunction(value){
-  //if(value in userName){
-  //  alert('This name is taken');
-  //}
-//}
-
-//function checkUser(){
- // let userName= document.getElementById('usrName').value;
-  //const names= [Harry,Elizabeth,Shana];
-  //for (let x in names){
-  //  if(userName.includes(names[x]))
-    //  alert('This name is taken')
-  //}
-//}
-
-function checkUser(){
-  let userName= document.getElementById('usrName').value;
-  const names= [Harry,Elizabeth,Shana];
-  if(names.includes(userName){
-      alert('This name is taken')
-  }
-}
