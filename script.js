e0f00ee43f90ca968768088aced37ab774d868aa
Jennifer Marshall <111827054+Jennijack89@users.noreply.github.com>
2022-10-21 17:46:53 +0000

    Added Canva/SVG/ Img/ Media Query (rough draft)

--- script.js
+++ script.js
@@ -18,9 +18,47 @@ function verifyPw(){
     }
   return true;
 }
-//function showPwd() {
- // let myInput=document.getElementsByTagName('input');
- // let rePassWord=document.getElementById('passWrdreEntr').value;
-  //if(myInput.type === "password") {
-//myInput.type = "text";}
-  //}
+
+/************* My Canvas Code***************/
+
+//gets the Canva element by id
+var canvas = document.getElementById('mycanvas');
+//makes sure we don't execute when canvas isn't supported
+/*if (canvas.getContext) {   */
+  //allows us to use canva for deawing
+var ctx = canvas.getContext('2d');   
+   // drawing code here  
+    ctx.fillStyle ='rgb(0,40,30)';
+    ctx.fillRect(0,0,100,100);
+
+    ctx.fillStyle ='rgb(0,100,20)';
+    ctx.fillRect(100,25,100,100);
+
+    ctx.fillStyle ='rgb(0,40,10)';
+    ctx.fillRect(200,100,120,100);
+
+    ctx.fillStyle ='rgb(255,0,30)';
+    ctx.fillRect(300,0,40,50);
+    
+    ctx.clearRect(30,5,20,25);
+
+    ctx.strokeRect(70,70,70,70);
+
+  //line
+    ctx.beginPath();
+    ctx.moveTo(140,40);
+    ctx.lineTo(250,25);
+    ctx.strokeStyle = 'purple';
+    ctx.lineTo(350,70);
+    ctx.strokeStyle = "white";
+    ctx.stroke();
+
+  //arc
+    ctx.beginPath();
+    ctx.arc(100,50, 5,0,Math.PI = 2, false);
+    ctx.stoke();
+/*}else{
+   alert('You need Safari or Firefox 1.5+ to see this demo.');
+}
+}*/
+
