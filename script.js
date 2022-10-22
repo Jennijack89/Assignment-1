f43e2c3a154c0b283ac030bba782a23050c14b4b
Jennifer Marshall <111827054+Jennijack89@users.noreply.github.com>
2022-10-22 21:30:49 +0000

    Canva-Text Javascript <svg>-Image, Accessibility, Aria, Alt, tab-index,

--- script.js
+++ script.js
@@ -27,36 +27,38 @@ var canvas = document.getElementById('mycanvas');
 /*if (canvas.getContext) {   */
   //allows us to use canva for deawing
 var ctx = canvas.getContext('2d');   
-   // drawing code here  
-    ctx.fillStyle ='rgb(0,40,30)';
+   // drawing code here 
+  //Square Deisgn
+    ctx.fillStyle ='rgba(94,193,118,.6)'; //Filled square 1
     ctx.fillRect(0,0,100,100);
 
-    ctx.fillStyle ='rgb(0,100,20)';
+    ctx.fillStyle ='rgba(0,0,0,.5)';// Filled square 2
     ctx.fillRect(100,25,100,100);
 
-    ctx.fillStyle ='rgb(0,40,10)';
-    ctx.fillRect(200,100,120,100);
-
-    ctx.fillStyle ='rgb(255,0,30)';
-    ctx.fillRect(300,0,40,50);
+    ctx.fillStyle ='rgba(0,0,0,.6)';// Filled square 3
+    ctx.fillRect(200,50,120,100);
     
-    ctx.clearRect(30,5,20,25);
+    ctx.clearRect(80,40,40,40); //Clear square 1
+    ctx.clearRect(180,65,40,40);// clear Square 2
 
-    ctx.strokeRect(70,70,70,70);
+    ctx.strokeRect(70,30,60,60);//Hollow Square 1
+    ctx.strokeRect(170,55,60,60);//Hlear square 2
 
+  // text Design
+    ctx.fillStyle = 'white';
+    ctx.font = 'Bold 30px Papyrus';
+               
+    ctx.textBaseline = 'Top';
+    ctx.fillText("The Sky's the limit", 40, 100);
+               
   //line
-    ctx.beginPath();
+   /* ctx.beginPath();
     ctx.moveTo(140,40);
     ctx.lineTo(250,25);
     ctx.strokeStyle = 'purple';
     ctx.lineTo(350,70);
     ctx.strokeStyle = "white";
-    ctx.stroke();
-
-  //arc
-    ctx.beginPath();
-    ctx.arc(100,50, 5,0,Math.PI = 2, false);
-    ctx.stoke();
+    ctx.stroke();*/
 /*}else{
    alert('You need Safari or Firefox 1.5+ to see this demo.');
 }
